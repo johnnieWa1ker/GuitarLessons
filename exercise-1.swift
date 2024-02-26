d2bb0e64b582427abe03ab6553f34aeb9eacd486
Johnnie Walker <rockwave.fm@gmail.com>
2024-02-26 11:17:39 +0300

    Add first exercise

---
+++ exercise-1.swift
@@ -0,0 +1,82 @@
+
+STRIKES ON STRINGS: STRAIGHT STROKE, 6th string MUTTERED:
+
+
+6th string   0-0-0-0    0-0-0     0-0     0
+5th string         8        5       3     2
+
+6th string   0-0-0-0    0-0-0     0-0     0
+5th string         8        5       3     2
+
+6th string   0-0-0-0    0-0-0     0-0     0
+5th string         2        3       5     8
+
+6th string   0-0-0-0    0-0-0     0-0     0
+5th string         2        3       5     8
+
+// _______________________________________
+
+6th string   0-0-0-0    0-0-0     0-0     0
+5th string         8        5       3     2
+
+6th string   0-0-0-0    0-0-0     0-0     0
+5th string         8        5       3     2
+
+6th string   0-0-0-0    0-0-0     0-0     0
+5th string         2        3       5     8
+
+6th string   0-0-0-0    0-0-0     0-0     0
+5th string         2        3       5     8
+
+// _______________________________________
+
+6th string   0-0-0-0    0-0-0     0-0     0
+5th string         8        5       3     2
+
+6th string   0-0-0-0    0-0-0     0-0     0
+5th string         8        5       3     2
+
+6th string   0-0-0-0    0-0-0     0-0     0
+5th string         2        3       5     8
+
+6th string   0-0-0-0    0-0-0     0-0     0
+5th string         2        3       5     8
+
+// _______________________________________
+
+6th string   0-0-0        0-0       0     0
+5th string       8          5       3     2
+
+6th string   0-0-0        0-0       0     0
+5th string       8          5       3     2
+
+6th string   0-0-0        0-0       0     0
+5th string       2          3       5     8
+
+6th string   0-0-0        0-0       0     0
+5th string       2          3       5     8
+
+// _______________________________________
+
+6th string   0-0          0-0       0     0
+5th string     8            5       3     2
+
+6th string   0-0          0-0       0     0
+5th string     2            3       5     8
+
+6th string   0-0          0-0       0     0
+5th string     2            3       5     8
+
+// _______________________________________
+
+6th string   0-0          0-0     0-0   0-0
+5th string     8            5       3     2
+
+6th string   0-0          0-0     0-0   0-0
+5th string     2            3       5     8
+
+6th string     0            0       0     0
+5th string     8            5       3     2
+
+6th string     0            0       0     0
+5th string     2            3       5     8
